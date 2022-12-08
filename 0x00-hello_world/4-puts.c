5290cb44970e21db58003be0770ad46b76a32d2d
richycasidy <richycasidy@gmail.com>
2022-12-08 15:31:26 -0800

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
+
+/**
+ * main - Entry point
  *
- * Return: 0
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
