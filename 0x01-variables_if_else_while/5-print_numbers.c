c20613855478eed8cf5b0c2ed142c2c35070c991
richycasidy <richycasidy@gmail.com>
2022-12-09 14:45:48 -0800

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - So betty about my proposal
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - so betty, just say yes e no hard
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
