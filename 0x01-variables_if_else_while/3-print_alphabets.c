dd825e2ca4bfec32d913e43c000fbfdf5c9c98b1
richycasidy <richycasidy@gmail.com>
2022-12-09 13:27:53 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Hey big betty, I am ready for you
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
