5bce9e9623f0da30cc77007ecbdc8d8e90e7f5f6
richycasidy <richycasidy@gmail.com>
2022-12-09 15:00:12 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/**
+ * main - so betty where are you taking me to
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+	putchar(10)
+
+	return (0);
+}
