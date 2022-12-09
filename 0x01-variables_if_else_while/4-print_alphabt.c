6f4d73c8ceb148c2d841383abb6703014e2b934f
richycasidy <richycasidy@gmail.com>
2022-12-09 14:13:08 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,25 @@
-nkjndfjnmxc
+#include <stdio.h>
+/**
+ * main - So betty can we now be friends
+ *
+ * code: by Richard
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
