b5e69c6fa38396b846c894082fa72c746e74672b
richycasidy <richycasidy@gmail.com>
2022-12-09 12:42:15 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
- * main - big betty
+ * main - Entry point
+ *
+ * Description: print last diit in random number
  *
  * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	int n last;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
