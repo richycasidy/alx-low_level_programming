98372e6f9daba27499e72315887d7ecfacb42390
richycasidy <richycasidy@gmail.com>
2023-01-04 15:03:59 -0800

    task correctionn

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,60 @@
 #include "main.h"
 
-int check_palindrome(char *s);
-
 /**
-  * is_palindrome - Returns if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int is_palindrome(char *s)
+ * length - returns length of string
+ * @s: string
+ * Return: integer length
+ */
+
+int length(char *s)
 {
-	if (*s == '0')
-		return (1);
 
-	return (check_palindrome(s));
-}
-
-/**
-  * check_palindrome - Check if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
+	if (*s != '\0')
 	{
-		s++;
-		l--;
+		return (1 + length(s + 1));
 	}
 	else
 	{
 		return (0);
 	}
-
-	return (1);
 }
 
 /**
-  * _strlen_recursion - Get the length of a string
-  * @s: the string to get the length
-  *
-  * Return: the string length
-  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
+ * compare - compare characters
+ * @s: string
+ * @beg: beginning index
+ * @end: end index
+ * Return: integer indicating non-match 0, match 1
+ */
 
-	s++;
-	return (_strlen_recursion(s) + 1);
+int compare(char *s, int beg, int end)
+{
+	if (s[beg] != s[end])
+		return (0);
+	else if (beg == end && s[beg] == s[end])
+		return (1);
+	else if (beg == end - 1 && s[beg] == s[end])
+		return (1);
+	else
+		return (compare(s, beg + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks is string is a palindrome
+ * @s: string
+ * Return: integer, 1 if palindrom, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int len, beg, end;
+
+	len = length(s);
+	beg = 0;
+	end = len - 1;
+
+	if (len == 0 || len == 1)
+		return (1);
+	else
+		return (compare(s, beg, end));
 }
