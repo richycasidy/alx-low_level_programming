af77130bba0740c9ff105741b00c289d9b077842
Richard Casidy <36550303+richycasidy@users.noreply.github.com>
2023-01-10 18:31:59 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,17 +2,29 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: int size of memory to be allocated
- * Return: pointer to allocated memory
- */
-
-void *malloc_checked(unsigned int b)
+  * create_array - creates an array of chars
+  * @size: The size of the array
+  * @c: The char to fill in the array
+  *
+  * Return: The array filled
+  */
+char *create_array(unsigned int size, char c)
 {
-	void *ptr;
+	unsigned int i;
+	char *s;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
 }
