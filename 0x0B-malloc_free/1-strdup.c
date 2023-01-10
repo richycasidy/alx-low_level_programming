97165e3bb1317f40d87cc09ad60706daec2f1845
Richard Casidy <36550303+richycasidy@users.noreply.github.com>
2023-01-10 18:33:29 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,66 +2,35 @@
 #include <stdlib.h>
 
 /**
- * _strlen - gets string length
- * @string: string
- * Return: length (int)
- */
-
-int _strlen(char *string)
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
+  *
+  * Return: the string duplicated
+  */
+char *_strdup(char *str)
 {
-	int length;
+	int a = 0, i = 1;
+	char *s;
 
-	length = 0;
-	while (string[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
-
-/**
- * string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: integer that determines whether entire s2 string will be used
- * Return: pointer to allocated memory corresponding with the string
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int length1, length2, lengthc, i, j;
-	char *str;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	length1 = _strlen(s1);
-	length2 = _strlen(s2);
-
-	if (n >= length2)
-		lengthc = length1 + length2 + 1;
-	else
-	{
-		lengthc = length1 + n + 1;
-		length2 = n;
-	}
-	str = malloc(lengthc);
 	if (str == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < length1)
+	while (str[i])
 	{
-		str[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (j < length2)
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
 	{
-		str[i + j] = s2[j];
-		j++;
+		s[a] = str[a];
+		a++;
 	}
-	str[i + j] = '\0';
-	return (str);
+
+	s[a] = '\0';
+	return (s);
 }
